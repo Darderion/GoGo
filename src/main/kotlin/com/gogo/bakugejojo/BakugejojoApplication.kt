2b6e6611f41ca2fd3a70aa04c8319bb19da7c721
Darderion <Eobard1SA@gmail.com>
2020-12-28 16:54:34 +0300

    Created a repository

---
+++ src/main/kotlin/com/gogo/bakugejojo/BakugejojoApplication.kt
@@ -0,0 +1,11 @@
+package com.gogo.bakugejojo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BakugejojoApplication
+
+fun main(args: Array<String>) {
+	runApplication<BakugejojoApplication>(*args)
+}
