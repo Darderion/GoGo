a7b760b3f60ab70ea7414dc2e2a3e3bad6dbb551
Nwors <8grisha8@mail.ru>
2021-01-05 19:39:25 +0500

    Added character selector

---
+++ src/main/resources/static/images/images.js
@@ -0,0 +1,17 @@
+
+let imagesUrls = {
+	profileImages : {
+		dio : 'https://media.discordapp.net/attachments/635510698209837068/796008953447579688/DioProfile.png',
+		jotaro : 'https://media.discordapp.net/attachments/635510698209837068/796008968191344650/JotaroProfile.png',
+		delamain : 'https://media.discordapp.net/attachments/635510698209837068/796008944513318912/delamain.png'
+	},
+
+	blockImages : {
+		bomb : 'https://cdn.discordapp.com/attachments/635510698209837068/796008939555782676/bomb.png',
+		explosion : 'https://media.discordapp.net/attachments/635510698209837068/796008962721579058/explosion.png',
+		wall : 'https://cdn.discordapp.com/attachments/635510698209837068/796008969881124874/block.png'
+	},
+
+	empty : 'https://media.discordapp.net/attachments/635510698209837068/796008957507665940/empty.png'
+
+}
